86b0fee29a6602bf1e3586dafbe9912c3dce55bf
noel2017mdala <noelmdala2017@gmail.com>
2021-10-27 03:55:22 +0200

    add default text when their is no coversatioin in chat

--- src/pages/Chat/components/Convo.jsx
+++ src/pages/Chat/components/Convo.jsx
@@ -51,89 +51,95 @@ const Convo = ({ lastMsgRef, messages, messageEndRef, scrollFunction }) => {
 
   return (
     <div>
-      {!messages
-        ? null
-        : messages.map((messagesData, index) => (
-            <div key={index}>
-              <div className="chat__date-wrapper">
-                {checkDate(messagesData.dateSent) ? null : getToday() ===
-                  holdValue ? (
-                  <span className="chat__date">TODAY</span>
-                ) : (
-                  <span className="chat__date"> {holdValue}</span>
-                )}
-              </div>
-              {index === 0 && (
-                <p className="chat__encryption-msg">
-                  <Icon id="lock" className="chat__encryption-icon" />
-                  Messages are end-to-end encrypted. No one outside of this
-                  chat, not even WhatsApp, can read or listen to them. Click to
-                  learn more.
-                </p>
-              )}
+      {!messages ? null : messages.length < 1 ? (
+        <>
+          <div className="chat__date-wrapper">
+            <span className="chat__date">TODAY</span>
+          </div>
 
-              <div className="chat__msg-group">
-                <>
-                  {messagesData.attachedImage ? (
-                    "Image Was Attached"
-                  ) : messagesData.from !== userData._id ? (
-                    <p
-                      className="chat__msg chat__msg--rxd"
-                      ref={assignRef(index, messages)}
-                    >
-                      <span>{messagesData.messagesBody}</span>
-                      <span className="chat__msg-filler"> </span>
-                      <span className="chat__msg-footer">
-                        {formatTime(messagesData.timeSent)}
-                      </span>
-                      <button
-                        aria-label="Message options"
-                        className="chat__msg-options"
-                      >
-                        <Icon
-                          id="downArrow"
-                          className="chat__msg-options-icon"
-                        />
-                      </button>
-                    </p>
-                  ) : (
-                    <p
-                      className="chat__msg chat__msg--sent"
-                      ref={assignRef(index, messages)}
-                    >
-                      <span>{messagesData.messagesBody}</span>
-                      <span className="chat__msg-filler"> </span>
-                      <span className="chat__msg-footer">
-                        <span> {formatTime(messagesData.timeSent)} </span>
-                        <Icon
-                          id={
-                            messagesData?.messageStatus === "sent"
-                              ? "singleTick"
-                              : "doubleTick"
-                          }
-                          aria-label={messagesData?.messageStatus}
-                          className={`chat__msg-status-icon ${
-                            messagesData?.messageStatus === "read"
-                              ? "chat__msg-status-icon--blue"
-                              : ""
-                          }`}
-                        />
-                      </span>
-                      <button
-                        aria-label="Message options"
-                        className="chat__msg-options"
-                      >
-                        <Icon
-                          id="downArrow"
-                          className="chat__msg-options-icon"
-                        />
-                      </button>
-                    </p>
-                  )}
-                </>
-              </div>
+          <p className="chat__encryption-msg">
+            <Icon id="lock" className="chat__encryption-icon" />
+            Messages are end-to-end encrypted. No one outside of this chat, not
+            even WhatsApp, can read or listen to them. Click to learn more.
+          </p>
+        </>
+      ) : (
+        messages.map((messagesData, index) => (
+          <div key={index}>
+            <div className="chat__date-wrapper">
+              {checkDate(messagesData.dateSent) ? null : getToday() ===
+                holdValue ? (
+                <span className="chat__date">TODAY</span>
+              ) : (
+                <span className="chat__date"> {holdValue}</span>
+              )}
             </div>
-          ))}
+            {index === 0 && (
+              <p className="chat__encryption-msg">
+                <Icon id="lock" className="chat__encryption-icon" />
+                Messages are end-to-end encrypted. No one outside of this chat,
+                not even WhatsApp, can read or listen to them. Click to learn
+                more.
+              </p>
+            )}
+
+            <div className="chat__msg-group">
+              <>
+                {messagesData.attachedImage ? (
+                  "Image Was Attached"
+                ) : messagesData.from !== userData._id ? (
+                  <p
+                    className="chat__msg chat__msg--rxd"
+                    ref={assignRef(index, messages)}
+                  >
+                    <span>{messagesData.messagesBody}</span>
+                    <span className="chat__msg-filler"> </span>
+                    <span className="chat__msg-footer">
+                      {formatTime(messagesData.timeSent)}
+                    </span>
+                    <button
+                      aria-label="Message options"
+                      className="chat__msg-options"
+                    >
+                      <Icon id="downArrow" className="chat__msg-options-icon" />
+                    </button>
+                  </p>
+                ) : (
+                  <p
+                    className="chat__msg chat__msg--sent"
+                    ref={assignRef(index, messages)}
+                  >
+                    <span>{messagesData.messagesBody}</span>
+                    <span className="chat__msg-filler"> </span>
+                    <span className="chat__msg-footer">
+                      <span> {formatTime(messagesData.timeSent)} </span>
+                      <Icon
+                        id={
+                          messagesData?.messageStatus === "sent"
+                            ? "singleTick"
+                            : "doubleTick"
+                        }
+                        aria-label={messagesData?.messageStatus}
+                        className={`chat__msg-status-icon ${
+                          messagesData?.messageStatus === "read"
+                            ? "chat__msg-status-icon--blue"
+                            : ""
+                        }`}
+                      />
+                    </span>
+                    <button
+                      aria-label="Message options"
+                      className="chat__msg-options"
+                    >
+                      <Icon id="downArrow" className="chat__msg-options-icon" />
+                    </button>
+                  </p>
+                )}
+              </>
+            </div>
+          </div>
+        ))
+      )}
       <div ref={messageEndRef} />
     </div>
   );
